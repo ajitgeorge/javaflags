d9887e0e62962e83a90d049fa54d871debab4de9
ageorge <ageorge@lmd-ageorge01.us.drwholdings.com>
2010-07-13 02:20:46 -0500

    assign string values

--- src/main/java/com/drw/flags/Flags.java
+++ src/main/java/com/drw/flags/Flags.java
@@ -1,12 +1,50 @@
 package com.drw.flags;
 
+import com.google.common.base.Predicate;
 import org.reflections.Reflections;
+import org.reflections.scanners.FieldAnnotationsScanner;
 
+import java.lang.reflect.Field;
+import java.util.List;
 import java.util.Set;
 
+import static com.google.common.collect.Iterables.filter;
+import static com.google.common.collect.Iterables.getOnlyElement;
+import static com.google.common.collect.Lists.newArrayList;
+
 public class Flags {
-    public Set<Class<? extends Flags>> subclasses() {
-        Reflections reflections = new Reflections("com.drw.flags");
-        return reflections.getSubTypesOf(Flags.class);        
+    private Reflections reflections;
+
+    public Flags(String packagePrefix) {
+        reflections = new Reflections(packagePrefix, new FieldAnnotationsScanner());
+    }
+
+    public List<String> parse(String[] argv) {
+        List<String> nonFlagArguments = newArrayList();
+
+        Set<Field> flaggedFields = reflections.getFieldsAnnotatedWith(Flag.class);
+
+        for (String s : argv) {
+            if (s.startsWith("--")) {
+                String[] parts = s.split("=", 2);
+                final String name = parts[0].substring(2);
+                String value = parts[1];
+
+                Field field = getOnlyElement(filter(flaggedFields, new Predicate<Field>() {
+                    @Override
+                    public boolean apply(Field elem) {
+                        return elem.getAnnotation(Flag.class).value().equals(name);
+                    }
+                }));
+                try {
+                    field.set(null, value);
+                } catch (IllegalAccessException e) {
+                    e.printStackTrace();
+                }
+            } else {
+                nonFlagArguments.add(s);
+            }
+        }
+        return nonFlagArguments;
     }
 }
--- src/test/java/com/drw/flags/FlagsTests.java
+++ src/test/java/com/drw/flags/FlagsTests.java
@@ -2,11 +2,22 @@ package com.drw.flags;
 
 import org.junit.Test;
 
-import static junit.framework.Assert.assertTrue;
+import java.util.List;
+
+import static com.google.common.collect.Iterables.getOnlyElement;
+import static junit.framework.Assert.assertEquals;
 
 public class FlagsTests {
     @Test
-    public void someTest() {
-        assertTrue(new Flags().subclasses().isEmpty());        
+    public void shouldParseArgumentsForFlagValues() {
+        assertEquals("default", Sample.stringFlag);
+
+        Flags flags = new Flags("com.drw.flags");
+        List<String> arguments = flags.parse(new String[]{"--string=something", "anonflagvalue"});
+
+        assertEquals(1, arguments.size());
+        assertEquals("anonflagvalue", getOnlyElement(arguments));
+
+        assertEquals("something", Sample.stringFlag);
     }
 }
